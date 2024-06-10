4a0640ccbc852e86f691b0ee4dc829dc267e35a2
m0n1b <161561830+m0n1b@users.noreply.github.com>
2024-06-10 23:04:25 +0800

    Add files via upload

---
+++ awvs.py
@@ -0,0 +1,215 @@
+import json
+import time
+import sys
+import requests
+import argparse
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning) 
+class awvs_api:
+   
+    def __init__(self,self_host,api_key,max_scan=10,wait_time=60):
+        self.max_scan=max_scan
+        self.wait_time=wait_time
+        self.pool_list=[]
+        self.self_host=self_host
+        dashbord_url = self_host+'/api/v1/me/stats' # 基本信息面板
+        self.dashbord_url=dashbord_url
+        add_scan_url = self_host+'/api/v1/scans' # 添加扫描url
+        self.add_scan_url=add_scan_url
+        self.api_key=api_key
+        self.total_target_url = self.self_host+'/api/v1/targets' 
+
+    def get_scans_num(self,url, headers):
+
+        '''
+        获取当前正在扫描的目标数
+        scans_running_count 正在扫描的个数
+        scans_conducted_count 扫完的个数
+        targets_count 总个数
+        '''
+
+        r = requests.get(url=url, headers=headers, verify=False).text
+        my_dict = json.loads(r)
+        return int(my_dict['scans_running_count'])
+
+
+    def get_target(self,target_file): 
+        '''
+        从本地获取url
+        '''
+        try:
+            with open(target_file, "r", encoding='utf-8') as f:
+                content = f.read().splitlines()
+                content = list(set(content))
+            return content
+        except Exception as e:
+            print("\n未找到文件！\n")
+            sys.exit(0)
+
+
+    def add_targets(self,url_list, headers, total_target_url,zican_name):
+        '''
+        添加目标
+        '''
+        count = 1
+        target_id = []
+
+        r = requests.get(url = total_target_url, headers = headers, verify = False).text
+        
+         
+        for i in url_list:
+            data = {
+                "address" : str(i),
+                "description" : zican_name,
+                "criticality" : "20"
+            }
+            r = requests.post(url = total_target_url, headers = headers, verify = False, data = json.dumps(data)).text
+            my_dict = json.loads(r)
+            target_id.append(my_dict["target_id"])
+            count += 1
+            print(my_dict)
+        #print('\n本次添加了' + str(count-1) + '个目标\n')
+        return target_id
+
+
+    def add_scans(self,add_scan_url, headers, total_target_url, count, target_id,max_num=10,sleep_num=60):
+        '''
+        添加扫描目标
+        '''
+        count = count
+        for i in target_id: # 速度设置
+            url = self.self_host+'/api/v1/targets/' + str(i) + '/configuration'
+            data = {
+                    "scan_speed":"moderate"
+                }
+            r = requests.patch(url = url, data = json.dumps(data), headers = headers, verify = False)
+            scans_num = self.get_scans_num(self.dashbord_url, headers) # 获取当前扫描数
+            
+            while int(scans_num) >= int(max_num):
+                print('\n当前扫描数已超过最大设定值，等待' + str(sleep_num) + '秒后再次扫描。\n')
+                time.sleep(int(sleep_num))
+                scans_num = self.get_scans_num(self.dashbord_url, headers)
+           
+            data = {
+                    "target_id": str(i),
+                    "profile_id": "11111111-1111-1111-1111-111111111111",
+                    "schedule": {
+                                    "disable":False,
+                                    "start_date":None,
+                                    "time_sensitive":False
+                            }
+                }
+            r = requests.post(url = add_scan_url, data = json.dumps(data), headers = headers, verify = False).text
+            count += 1
+            
+            print('添加第' + str(count) + '个目标扫描。\n')
+            time.sleep(1)
+            if count % int(max_num) == 0:
+                print('检查扫描数中，等待十秒。。\n')
+                time.sleep(10)
+        print('\n添加结束！\n')
+                
+    def im_add_scans(self,add_scan_url, headers, total_target_url, count, target_id,max_num=10,sleep_num=60):
+        '''
+        添加扫描目标
+        '''
+        for i in target_id: # 速度设置
+            url = self.self_host+'/api/v1/targets/' + str(i) + '/configuration'
+            data = {
+                    "scan_speed":"moderate"
+                }
+            r = requests.patch(url = url, data = json.dumps(data), headers = headers, verify = False)
+            scans_num = self.get_scans_num(self.dashbord_url, headers) # 获取当前扫描数
+            
+           
+            data = {
+                    "target_id": str(i),
+                    "profile_id": "11111111-1111-1111-1111-111111111111",
+                    "schedule": {
+                                    "disable":False,
+                                    "start_date":None,
+                                    "time_sensitive":False
+                            }
+                }
+            r = requests.post(url = add_scan_url, data = json.dumps(data), headers = headers, verify = False).text
+            
+            
+            #print('添加第' + str(count) + '个目标扫描。\n')
+            time.sleep(1)
+            
+    def im_add_scans_out(self, target_list):
+        '''
+        添加扫描目标
+        '''
+        headers = {
+        'X-Auth': self.api_key,
+        'Content-type': 'application/json'
+        }
+        target_id = self.add_targets(target_list, headers, self.total_target_url,"im_add_scans_api")
+        for i in target_id: # 速度设置
+            url = self.self_host+'/api/v1/targets/' + str(i) + '/configuration'
+            data = {
+                    "scan_speed":"moderate"
+                }
+            r = requests.patch(url = url, data = json.dumps(data), headers = headers, verify = False)
+            scans_num = self.get_scans_num(self.dashbord_url, headers) # 获取当前扫描数
+            
+           
+            data = {
+                    "target_id": str(i),
+                    "profile_id": "11111111-1111-1111-1111-111111111111",
+                    "schedule": {
+                                    "disable":False,
+                                    "start_date":None,
+                                    "time_sensitive":False
+                            }
+                }
+            r = requests.post(url = self.add_scan_url, data = json.dumps(data), headers = headers, verify = False).text
+           
+            
+            #print('添加第' + str(count) + '个目标扫描。\n')
+            time.sleep(1)  
+        
+    def begin(self,t_list,z_name,max_num=10,sleep_num=60):
+        headers = {
+        'X-Auth': self.api_key,
+        'Content-type': 'application/json'
+        }
+        total_target_url = self.self_host+'/api/v1/targets' # 获取所有目标信息
+        dashbord_url = self.self_host+'/api/v1/me/stats' # 基本信息面板
+        add_scan_url = self.self_host+'/api/v1/scans' # 添加扫描url
+        count = 0
+        target_list = t_list # 获取url列表
+        target_id = self.add_targets(target_list, headers, total_target_url,z_name)
+        self.add_scans(add_scan_url, headers, total_target_url, count, target_id,max_num,sleep_num)
+        
+    def add_pool(self,url):
+        if not hasattr(self, 'pool_list'):
+            self.pool_list = []
+        if isinstance(url, str):
+            self.pool_list.append(url)
+        elif isinstance(url, list):
+            self.pool_list.extend(url)
+        else:
+            pass
+        self.pool_list = list(set(self.pool_list))
+    def get_list_num(self):
+        return len(self.pool_list)
+    def pool_scan(self):
+        #url = self.self_host+'/api/v1/targets/' + str(i) + '/configuration'
+        headers = {
+            'X-Auth': self.api_key,
+            'Content-type': 'application/json'
+            }
+        scans_num = self.get_scans_num(self.dashbord_url, headers) # 获取当前扫描数
+            
+        while True:
+            scans_num = self.get_scans_num(self.dashbord_url, headers)
+            if (int(scans_num) >= int(self.max_scan)) and len(self.pool_list)>0:   
+                next_scan=self.pool_list.pop(0)
+                target_id = self.add_targets([next_scan], headers, self.total_target_url,f"api_pool_{int(time.time())}")
+                self.im_add_scans(add_scan_url, headers, self.total_target_url, 0, target_id,self.max_scan,self.wait_time)
+            print(f"now scan {scans_num}")
+            time.sleep(self.wait_time)    
+
+
---
+++ telegram.py
@@ -0,0 +1,185 @@
+from flask import Flask, request, jsonify
+import requests,threading
+from awvs import awvs_api  
+from oneforall import OneForAll
+from io import BytesIO
+
+
+app = Flask(__name__)
+
+TELEGRAM_URL = "https://api.telegram.org/bot"
+TOKEN = "7029587164:AAFbWYN1_i0O_VYy1uaiX5rN8PxB8ahcue8"
+
+awvs_url='https://20.41.115.237:3443/'
+awvs_key='104fd594feeab42bd9505873633460c55aa464d4c66d24297b8a32d08613f2d7d'
+awvs_scan = awvs_api(awvs_url,awvs_key)
+thread = threading.Thread(target=awvs_scan.pool_scan)
+thread.start()
+
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    chat_id = req['message']['chat']['id']
+    text = req['message']['text']
+    print(text)
+    return_text=handler_req(chat_id, text)
+    send_message(chat_id, return_text)
+    
+    return jsonify(success=True)
+
+
+def oneforall(domain):
+    test = OneForAll(target=domain)
+    test.dns = True
+    test.brute = True
+    test.req = True
+    test.takeover = True
+    test.alive = True
+    test.run()
+    results = test.datas
+    #print(results)
+    a=[]
+    for url in results:
+        #print(url['url'])  
+        a.append("url:"+url['url']+"   title:"+url['title'][:30]+"   ip:"+url['ip']   )
+         
+    results = list(set(a))
+    return results
+
+def oneforall_scan_i(domain):
+    test = OneForAll(target=domain)
+    test.dns = True
+    test.brute = True
+    test.req = True
+    test.takeover = True
+    test.alive = True
+    test.run()
+    results = test.datas
+    #print(results)
+    a=[]
+    for url in results:
+        #print(url['url'])  
+        if( url['title']!=None ) or  (url['title']!="None" ):
+            a.append(url['url'])
+         
+    results = list(set(a))
+    return results   
+
+
+
+
+def oneforall_scan(url,chat_id):
+    result=oneforall_scan_i(url)
+    awvs_scan.add_pool(result)
+    now_num=awvs_scan.get_list_num()
+    text=f"{url}子域名扫描完成,扫描到{len(result)}子域名,并添加到awvs扫描队列成功 目前扫描队列有{now_num}"
+    send_message(chat_id, text)
+    
+def only_oneforall_scan(url,chat_id):
+    result=oneforall(url)
+    result_string = '\n'.join(result)
+    result_file = BytesIO(result_string.encode('utf-8'))
+    result_file.name = f"{url}.txt" 
+    send_file_to_telegram(chat_id, result_file)
+    
+def im_add_awvs_s(url,chat_id):
+    awvs_scan.im_add_scans_out([url])
+    text=f"立即{url}添加完成!请在awvs查看!"
+    send_message(chat_id, text)
+    
+def oneforall_scan_arr(url_list,chat_id):
+    for url in url_list:
+        result=oneforall_scan_i(url)
+        awvs_scan.add_pool(result)
+        now_num=awvs_scan.get_list_num()
+        
+    text=f"批量子域名扫描完成,完成了{len(url)}的扫描,并添加到awvs扫描队列成功 目前扫描队列有{now_num}"
+    send_message(chat_id, text)
+    
+    
+def handler_req(chat_id, text):
+    parts = text.split()
+    if len(parts) == 0:
+        return "输入不正确"
+    command = parts[0]
+    if command == 'help':
+        return "你现在可以使用我来进行半自动扫描! 目前的命令有  ljsm(立即扫描 将一个url地址立即添加到awvs扫描!)   zysm(子域扫描 首先爆破子域名  提取用title的子域名推送到awvs 但有队列)  lzysm(list子域名扫描 多个子域名扫描 使用,分割多个扫描 然后子域名 然后推awvs )  zym(单纯的子域名扫描  扫描完成后发个文件回来!) "
+    elif command == 'sm':
+        if len(parts) < 2:
+            return "参数不足"
+        awvs_scan.add_pool(parts[1])
+        now_num=awvs_scan.get_list_num()
+        return f"添加到队列成功 目前队列有{now_num}"
+    elif command=="zysm":
+        if len(parts) < 2:
+            return "参数不足"
+        now_num=awvs_scan.get_list_num()
+        zym_thread = threading.Thread(target=oneforall_scan,args=(parts[1],chat_id,))
+        zym_thread.start()
+        return f"正在运行子域名扫描并添加awvs,完成后会回复，目前队列有{now_num}"
+    elif command == 'ljsm':
+        if len(parts) < 2:
+            return "参数不足"
+        rpym_thread = threading.Thread(target=im_add_awvs_s,args=(parts[1],chat_id,))
+        rpym_thread.start()
+        return f"正在运行立即添加到awvs,完成后会回复!"
+    elif command == 'lzysm':
+        if len(parts) < 2:
+            return "参数不足"
+        items = parts[1].split(',')
+        pym_thread = threading.Thread(target=oneforall_scan_arr,args=(items,chat_id,))
+        pym_thread.start()
+        now_num=awvs_scan.get_list_num()
+        return f"正在运行子域名批量域名扫描并添加awvs,完成后会回复，目前队列有{now_num}"
+    elif command == 'zym':
+        if len(parts) < 2:
+            return "参数不足"
+        onlyym_thread = threading.Thread(target=only_oneforall_scan,args=(parts[1],chat_id,))
+        onlyym_thread.start()    
+        return f"正在运行子域名扫描,完成后会回复文件!"
+    else:
+        return "当前不支持"
+
+    
+    
+
+def send_message(chat_id, text):
+    url = f"{TELEGRAM_URL}{TOKEN}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    requests.post(url, json=payload)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    chat_id = request.form.get('chat_id')
+    if not chat_id:
+        return jsonify({"error": "No chat_id provided"}), 400
+    
+    # 发送文件到 Telegram
+    send_file_to_telegram(chat_id, file)
+    
+    return jsonify(success=True)
+
+def send_file_to_telegram(chat_id, file):
+    url = f"{TELEGRAM_URL}{TOKEN}/sendDocument"
+    files = {
+        'document': file, 
+        'chat_id': (None, str(chat_id)),
+    }
+    response = requests.post(url, files=files)
+
+if __name__ == '__main__':
+    
+    app.run(host="0.0.0.0",port=80)
